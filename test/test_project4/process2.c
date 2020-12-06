18ec4aa7ce2755dd92d3f048159fcd9bd79c8a47
Patrick <gezhongxin18@mails.ucas.ac.com>
2020-12-06 16:50:16 +0800

    ss

--- test/test_project4/process2.c
+++ test/test_project4/process2.c
@@ -16,12 +16,12 @@ int rand()
 
 int atoi(char *str)
 {
+    int ret = 0;
     int base = 16;
     if ((str[0] == '0' && str[1] == 'x') || (str[0] == '0' && str[1] == 'X')) {
         base = 16;
         str += 2;
     }
-    int ret = 0;
     while (*str != '\0') {
         if ('0' <= *str && *str <= '9') {
             ret = ret * base + (*str - '0');
