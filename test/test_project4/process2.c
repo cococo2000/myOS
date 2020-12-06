71ee8eb9d1309d4d26885c7e6804227a8c625b61
Patrick <gezhongxin18@mails.ucas.ac.com>
2020-12-06 16:04:07 +0800

    use blank

--- test/test_project4/process2.c
+++ test/test_project4/process2.c
@@ -20,33 +20,33 @@ int atoi(char *input)
 
 void rw_task1(char *argv[])
 {
-	int mem1, mem2 = 0;
-	int curs = 0;
-	int memory[RW_TIMES * 2];
+    int mem1, mem2 = 0;
+    int curs = 0;
+    int memory[RW_TIMES * 2];
 
-	int i = 0;
+    int i = 0;
 
-	// srand((uint32_t)get_ticks());
-	for (i = 0; i < RW_TIMES; i++)
-	{
-		sys_move_cursor(1, curs + i);
-		mem1 = atoi(argv[i + 2]);
+    // srand((uint32_t)get_ticks());
+    for (i = 0; i < RW_TIMES; i++)
+    {
+        sys_move_cursor(1, curs + i);
+        mem1 = atoi(argv[i + 2]);
 
-		memory[i] = mem2 = rand();
-		*(int *)mem1 = mem2;
-		printf("Write: 0x%x,%d", mem1, mem2);
-	}
-	curs = RW_TIMES;
-	for (i = 0; i < RW_TIMES; i++)
-	{
-		sys_move_cursor(1, curs + i);
-		mem1 = atoi(argv[RW_TIMES + i + 2]);
+        memory[i] = mem2 = rand();
+        *(int *)mem1 = mem2;
+        printf("Write: 0x%x,%d", mem1, mem2);
+    }
+    curs = RW_TIMES;
+    for (i = 0; i < RW_TIMES; i++)
+    {
+        sys_move_cursor(1, curs + i);
+        mem1 = atoi(argv[RW_TIMES + i + 2]);
 
-		memory[i + RW_TIMES] = *(int *)mem1;
-		if (memory[i + RW_TIMES] == memory[i])
-			printf("Read succeed: 0x%x,%d", mem1, memory[i + RW_TIMES]);
-		else
-			printf("Read error: 0x%x,%d", mem1, memory[i + RW_TIMES]);
-	}
-	sys_exit();
+        memory[i + RW_TIMES] = *(int *)mem1;
+        if (memory[i + RW_TIMES] == memory[i])
+            printf("Read succeed: 0x%x,%d", mem1, memory[i + RW_TIMES]);
+        else
+            printf("Read error: 0x%x,%d", mem1, memory[i + RW_TIMES]);
+    }
+    sys_exit();
 }
