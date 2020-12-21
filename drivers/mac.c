d7db076147606bcbd8d0d7940acbd007cc391c60
Patrick <gezhongxin18@mails.ucas.ac.com>
2020-12-21 12:18:26 +0800

    p5 start

--- drivers/mac.c
+++ drivers/mac.c
@@ -9,27 +9,42 @@ uint32_t mac_cnt = 0;
 
 uint32_t reg_read_32(uint64_t addr)
 {
-    return *((uint64_t *)addr);
+    return *((uint32_t *)addr);
 }
 uint32_t read_register(uint64_t base, uint64_t offset)
 {
     uint64_t addr = base + offset;
     uint32_t data;
 
-    data = *(volatile uint64_t *)addr;
+    data = *(volatile uint32_t *)addr;
     return data;
 }
 
 void reg_write_32(uint64_t addr, uint32_t data)
 {
-    *((uint64_t *)addr) = data;
+    *((uint32_t *)addr) = data;
+}
+
+static void s_reset(mac_t *mac) //reset mac regs will inrupt
+{
+    uint32_t time = 1000000;
+    reg_write_32(mac->dma_addr, 0x01);
+
+    while ((reg_read_32(mac->dma_addr) & 0x01))
+    {
+        reg_write_32(mac->dma_addr, 0x01);
+        while (time)
+        {
+            time--;
+        }
+    };
 }
 
 static void gmac_get_mac_addr(uint8_t *mac_addr)
 {
     uint32_t addr;
 
-    addr = read_register(GMAC_BASE_ADDR, GmacAddr0Low) ;
+    addr = read_register(GMAC_BASE_ADDR, GmacAddr0Low);
     mac_addr[0] = (addr >> 0) & 0x000000FF;
     mac_addr[1] = (addr >> 8) & 0x000000FF;
     mac_addr[2] = (addr >> 16) & 0x000000FF;
@@ -212,7 +227,7 @@ void print_tx_dscrb(mac_t *mac)
     printf("send buffer mac->saddr=0x%x ", mac->saddr);
     printf("mac->saddr_phy=0x%x ", mac->saddr_phy);
     printf("send discrb mac->td_phy=0x%x\n", mac->td_phy);
-#if 1
+#if 0
     desc_t *send = (desc_t *)mac->td;
     for (i = 0; i < 1; i++)
     {
@@ -244,75 +259,186 @@ void print_rx_dscrb(mac_t *mac)
 
 static uint32_t printf_recv_buffer(uint64_t recv_buffer)
 {
-    
+    uint32_t i, flag, n;
+    flag = 0;
+    n = 0;
+    for (i = 0; i < PSIZE; i++)
+    {
+        if ((*((uint32_t *)recv_buffer + i) != 0) && (*((uint32_t *)recv_buffer + i) != 0xf0f0f0f0) && (*((uint32_t *)recv_buffer + i) != 0xf0f0f0f))
+        {
+            if (*((uint32_t *)recv_buffer + i) != 0xffffff)
+            {
+                printf(" %x ", *((uint32_t *)recv_buffer + i));
+
+                if (n % 10 == 0 && n != 0)
+                {
+                    printf(" \n");
+                }
+                n++;
+                flag = 1;
+            }
+        }
+    }
+    return flag;
+}
+
+/**
+ * Clears all the pending interrupts.
+ * If the Dma status register is read then all the interrupts gets cleared
+ * @param[in] pointer to synopGMACdevice.
+ * \return returns void.
+ */
+void clear_interrupt()
+{
+    uint32_t data;
+    data = reg_read_32(DMA_BASE_ADDR + DmaStatus);
+    reg_write_32(DMA_BASE_ADDR + DmaStatus, data & 0x1ffff);
 }
 void mac_irq_handle(void)
 {
-     
 }
- 
+
 void irq_enable(int IRQn)
 {
-     
 }
 
 void mac_recv_handle(mac_t *test_mac)
 {
-     
 }
- 
+
 static uint32_t printk_recv_buffer(uint64_t recv_buffer)
 {
-    
+    uint32_t i, flag, n;
+    flag = 0;
+    n = 0;
+    for (i = 0; i < PSIZE; i++)
+    {
+        if ((*((uint32_t *)recv_buffer + i) != 0) && (*((uint32_t *)recv_buffer + i) != 0xf0f0f0f0) && (*((uint32_t *)recv_buffer + i) != 0xf0f0f0f))
+        {
+            if (*((uint32_t *)recv_buffer + i) != 0xffffff)
+            {
+                printk(" %x ", *((uint32_t *)recv_buffer + i));
+
+                if (n % 10 == 0 && n != 0)
+                {
+                    printk(" \n");
+                }
+                n++;
+                flag = 1;
+            }
+        }
+    }
+    return flag;
 }
 
 void set_sram_ctr()
 {
-    *((volatile uint64_t *)0xffffffffbfd00420) = 0x8000; /* 使能GMAC0 */
+    *((volatile uint32_t *)0xffffffffbfe10420) = 1; /* 使能GMAC0 DMA一致性 */
 }
-static void s_reset(mac_t *mac) //reset mac regs
-{
-    uint32_t time = 1000000;
-    reg_write_32(mac->dma_addr, 0x01);
 
-    while ((reg_read_32(mac->dma_addr) & 0x01))
-    {
-        reg_write_32(mac->dma_addr, 0x01);
-        while (time)
-        {
-            time--;
-        }
-    };
-}
 void disable_interrupt_all(mac_t *mac)
 {
-    
+    reg_write_32(mac->dma_addr + DmaInterrupt, DmaIntDisable);
+    return;
 }
-void set_mac_addr(mac_t *mac)
+
+static void mac_recv_desc_init(mac_t *mac)
+{
+}
+static void mii_dul_force(mac_t *mac)
+{
+    reg_write_32(mac->dma_addr, 0x80);
+
+    uint32_t conf = 0xc800;
+
+    reg_write_32(mac->mac_addr, reg_read_32(mac->mac_addr) | (conf) | (1 << 8));
+    //enable recieve all
+
+    reg_write_32(mac->mac_addr + 0x4, 0x80000001);
+}
+
+void dma_control_init(mac_t *mac, uint32_t init_value)
+{
+    reg_write_32(mac->dma_addr + DmaControl, init_value);
+    return;
+}
+void set_mac_addr(mac_t *mac, uint8_t *MacAddr)
 {
     uint32_t data;
-    uint8_t MacAddr[6] = {0x00, 0x55, 0x7b, 0xb5, 0x7d, 0xf7};
+
     uint32_t MacHigh = 0x40, MacLow = 0x44;
     data = (MacAddr[5] << 8) | MacAddr[4];
     reg_write_32(mac->mac_addr + MacHigh, data);
     data = (MacAddr[3] << 24) | (MacAddr[2] << 16) | (MacAddr[1] << 8) | MacAddr[0];
     reg_write_32(mac->mac_addr + MacLow, data);
 }
-uint32_t do_net_recv(uint64_t rd, uint64_t rd_phy, uint64_t daddr)
+
+static void mac_send_desc_init(mac_t *mac)
 {
-    
+}
+
+/* buf_addr is the total recv buffer's address; size means the total size of recv buffer;
+the total recv buffer may have some little recv buffer;
+num means recv buffer's num ;length is each small recv buffer's size*/
+uint32_t do_net_recv(uint64_t buf_addr, uint64_t size, uint64_t num, uint64_t length)
+{
+
+    mac_t mac;
+
+    mac.mac_addr = GMAC_BASE_ADDR;
+    mac.dma_addr = DMA_BASE_ADDR;
+
+    mac.psize = PSIZE * 4; // 128bytes
+    mac.pnum = PNUM;       // pnum
+    mac.daddr = buf_addr;
+    mac_recv_desc_init(&mac);
+    dma_control_init(&mac, DmaStoreAndForward | DmaTxSecondFrame | DmaRxThreshCtrl128);
+    clear_interrupt(&mac);
+
+    mii_dul_force(&mac);
+
+    reg_write_32(GMAC_BASE_ADDR, reg_read_32(GMAC_BASE_ADDR) | 0x4);
+    reg_write_32(DMA_BASE_ADDR + 0x18, reg_read_32(DMA_BASE_ADDR + 0x18) | 0x02200002); // start tx, rx
+    reg_write_32(DMA_BASE_ADDR + 0x1c, 0x10001 | (1 << 6));
+
+    reg_write_32(DMA_BASE_ADDR + 0x1c, DMA_INTR_DEFAULT_MASK);
+
+    /*  YOU NEED ADD RECV CODE*/
+
     return 0;
 }
 
-void do_net_send(uint64_t td, uint64_t td_phy)
+/* buf_addr is sended buffer's address; size means sended buffer's size;
+num means sended buffer's times*/
+void do_net_send(uint64_t buf_addr, uint64_t size, uint64_t num)
 {
-     
+    uint64_t td_phy;
+    mac_t mac;
+    mac.mac_addr = GMAC_BASE_ADDR;
+    mac.dma_addr = DMA_BASE_ADDR;
+
+    mac.psize = PSIZE * 4;
+    mac.pnum = PNUM;
+    td_phy = mac.td_phy;
+    mac.saddr = buf_addr;
+    mac_send_desc_init(&mac);
+    dma_control_init(&mac, DmaStoreAndForward | DmaTxSecondFrame | DmaRxThreshCtrl128);
+    clear_interrupt(&mac);
+
+    mii_dul_force(&mac);
+
+    reg_write_32(GMAC_BASE_ADDR, reg_read_32(GMAC_BASE_ADDR) | 0x8);                    // enable MAC-TX
+    reg_write_32(DMA_BASE_ADDR + 0x18, reg_read_32(DMA_BASE_ADDR + 0x18) | 0x02202000); //0x02202002); // start tx, rx
+    reg_write_32(DMA_BASE_ADDR + 0x1c, 0x10001 | (1 << 6));
+    reg_write_32(DMA_BASE_ADDR + 0x1c, DMA_INTR_DEFAULT_MASK);
+
+    /*  YOU NEED ADD SEND CODE*/
 }
 void set_mac_int()
 {
-    volatile uint64_t *entry_gmac0;
+    volatile uint8_t *entry_gmac0;
     entry_gmac0 = 0xffffffff1fe1140c | 0xa0000000;
-    *entry_gmac0 = 0x41; //0 core ip4 int2
+    *entry_gmac0 = 0x41; //0 core ip6 int2
 }
 void do_init_mac(void)
 {
@@ -324,11 +450,18 @@ void do_init_mac(void)
 
     test_mac.psize = PSIZE * 4; // 64bytes
     test_mac.pnum = PNUM;       // pnum
+    uint8_t mac_addr[6];
 
-   
+    gmac_get_mac_addr(mac_addr);
+    set_sram_ctr(); /* 使能GMAC0 */
+                    //   s_reset(&test_mac); //will interrupt
+    disable_interrupt_all(&test_mac);
+    set_mac_addr(&test_mac, mac_addr);
+    register_irq_handler(12, mac_irq_handle);
+    reg_write_32(GMAC_BASE_ADDR + 0X3C, 1);
+    set_mac_int();
 }
 
 void do_wait_recv_package(void)
 {
-     
 }
